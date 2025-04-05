64ef4e8390ab344e9b55485ae1e67b21fb398cff
asadbek000002 <asadbektuygunov9@gmail.com>
2025-04-05 15:02:31 +0500

    admin panel userlar togrlandi

--- employee/admin.py
+++ employee/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.models import Group
 
+# Bu orqali Group modelini admin paneldan olib tashlaymiz
+admin.site.unregister(Group)
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "role")
+class UserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    list_display = ("username", "role", "is_staff", "is_superuser")
     search_fields = ("username",)
-    list_filter = ("role",)
+    list_filter = ("role", "is_staff", "is_superuser")
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Rol", {"fields": ("role",)}),
+        ("Ruxsatlar", {"fields": ("is_staff", "is_superuser")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "role", "password1", "password2", "is_staff", "is_superuser")}
+        ),
+    )
+
+    # Bu orqali 'groups' va 'user_permissions' ko‘rinmaydi
+    filter_horizontal = ()
